4ba70b7c52d122fd9c7a0558baf63caab2bc59eb
Dixith Maram <dixithmaram@gmail.com>
2018-10-24 10:45:32 +0530

    Initial Commit

---
+++ kindPoints/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  kindPoints
+//
+//  Created by Deekshith Maram on 10/24/18.
+//  Copyright © 2018 Deekshith Maram. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
